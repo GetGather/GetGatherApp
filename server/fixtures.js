cbfc3880bb27a531650a66cee19a2f87a69fa51e
matmello <mat.mello93@gmail.com>
2016-05-06 21:08:13 -0300

    fixtures to be executed on startup

---
+++ server/fixtures.js
@@ -0,0 +1,40 @@
+if(Events.find().count()===0){
+
+  Events.insert({
+    name: "Baile de Favela",
+    image: "/baile.jpg",
+    description: "Baile funkkkk"
+  });
+
+  Events.insert({
+    name: "Fute no Park",
+    image: "/campo.png",
+    description: "Futebol de Amigos no Park da cidade"
+  });
+
+  Events.insert({
+    name: "Vila Mix",
+    image: "/safadao.png",
+    description: "Vila Mix fetival esse sabado"
+  });
+
+  Events.insert({
+    name: "Piquenique",
+    image: "/piquenique.png",
+    description: "Piquenique na Beira do lago"
+  });
+
+  Events.insert({
+    name: "Teatro",
+    image: "/teatro.jpg",
+    description: "Melhor peça do mundo"
+  });
+
+
+  Events.insert({
+    name: "Kart",
+    image: "/kart.jpg",
+    description: "Corridinha dos broder"
+  });
+
+}
