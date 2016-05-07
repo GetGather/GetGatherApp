c68a16ec857914a4d1de1b39ecde7c3b8aeac476
matmello <mat.mello93@gmail.com>
2016-05-06 21:08:20 -0300

    images

--- server/service.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-  service: 'facebook'
-});
-
-ServiceConfiguration.configurations.insert({
-  service: 'facebook',
-  appId: '890695361076806',
-  secret: '4c70aadd24b5d6ca69280a632d10e0e8'
-});
